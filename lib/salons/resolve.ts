b55a0696a8e54007bb21497d6e4652b9f8932d6a
Eines BEKRI <einesbekri@gmail.com>
2026-05-12 00:57:55 +0200

    Fix UUID salon detection

--- lib/salons/resolve.ts
+++ lib/salons/resolve.ts
@@ -1,4 +1,4 @@
-export const UUID_REGEX = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i
+export const UUID_REGEX = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i
 
 export interface SalonIdentity {
   id: string
